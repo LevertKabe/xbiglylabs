a7196249640541a4b2c2d4a430c207a9c509365e
LevertKBH <levert@kbh.co.za>
2026-08-05 11:15:31 +0200

    Fixing grammar errors

--- SQL and Data Transformation/question-c.sql
+++ SQL and Data Transformation/question-c.sql
@@ -2,12 +2,12 @@
 - Spark UI and query proiiling, this includes looking ath the DAG for specific stages
        Data stew - issues on GROUP By customer_id or PARTITION BY region on question-b or question-a
 
-- Delta table history or stats, checj if ANALYSE TABLE STATISTICS has been run ti ensure optoimised with bad join startegties
+- Delta table history or stats, check if ANALYSE TABLE STATISTICS has been run ti ensure optimized with bad join strategies
 
 2. Query optimisations 
-- Partition of the TRANSCATION table, I would partition the table by month which I derive from the transaction_tiemstamp column, this would assist with date-range filters. For example last 90 days quyestiuon
+- Partition of the TRANSCATION table, I would partition the table by month which I derive from the transaction_tiemstamp column, this would assist with date-range filters. For example last 90 days question
 
-- RUn query OPTIMIZE dbo.Transactions which will compatct the small files or use liquid clustering which is used on modern Databricks runtime
+- RUn query OPTIMIZE dbo.Transactions which will compact  the small files or use liquid clustering which is used on modern Databricks runtime
 
 - Query plan level, for table like CUSTOMER which is small dim table male sure its broadcast join  instead of shuffle-join
 
